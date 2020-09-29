60bfa76b1b7e99f4c481ba89ed116bf8c3996ea9
Satvir Sandhu <356Sandhu@gmail.com>
2020-09-29 18:36:00 -0400

    add menus

--- app/public/wp-content/themes/followandrew/functions.php
+++ app/public/wp-content/themes/followandrew/functions.php
@@ -1,6 +1,29 @@
 <?php
 
-function followandrew_register_styles(){
+// This adds dynamic title tag support
+function followandrew_theme_support()
+{
+  add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'followandrew_theme_support');
+
+
+function followandrew_menus()
+{
+  $locations = array(
+    'primary' => "Desktop Primary Left Sidebar",
+    'footer' => "Footer Menu Items"
+  );
+
+  register_nav_menus($locations);
+}
+
+add_action('init', 'followandrew_menus');
+
+
+function followandrew_register_styles()
+{
 
   $version = wp_get_theme()->get('Version');
 
@@ -12,16 +35,12 @@ function followandrew_register_styles(){
 add_action('wp_enqueue_scripts', 'followandrew_register_styles');
 
 
-function followandrew_register_scripts(){
+function followandrew_register_scripts()
+{
   wp_enqueue_script('followandrew-jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '3.4.1', true);
   wp_enqueue_script('followandrew-popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '1.16.0', true);
   wp_enqueue_script('followandrew-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), '4.4.1', true);
-  wp_enqueue_script('followandrew-script', get_template_directory_uri()."/assets/js/main.js", array(), '1.0', true);
-  
-} 
+  wp_enqueue_script('followandrew-script', get_template_directory_uri() . "/assets/js/main.js", array(), '1.0', true);
+}
 
 add_action('wp_enqueue_scripts', 'followandrew_register_scripts');
-
-?>
-
-
